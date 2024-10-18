826d6cc9045a09bea2108fbca4a470f26b870042
pulsone-nathaniel <njpulsone@gmail.com>
2024-10-18 09:27:23 -0400

    creating second blog post

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -6,24 +6,8 @@
 library(tidyverse)
 library(dplyr)
 
-<<<<<<< HEAD
-traffic_data <- read_csv(here::here("dataset", "Traffic stops in Rhode Island.csv"))
-traffic_data |>
-  head(10)
-
-## CLEAN the data
-traffic_data_clean <- traffic_data |>
-  filter(!is.na(driver_gender)|!is.na(driver_race))|>
-  select(2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-
-#   pivot_longer(2:5, names_to = "group", values_to = "refusal_rate")
-# 
-# write_rds(loan_data_clean, file = here::here("dataset", "loan_refusal_clean.rds"))
-=======
-
 # Load data
 traffic_data <- read_csv(here::here("dataset", "Traffic stops in Rhode Island.csv"))
-
 
 # Clean data
 traffic_data_cleaned <- traffic_data %>%
@@ -37,8 +21,6 @@ traffic_data_cleaned <- traffic_data %>%
            !is.na(drugs_related_stop)) %>%
   select(-state, -county_name)
 
-
 # Save cleaned data as an .rds file
 # How to call data later: cleaned_data <- read_rds(here::here("dataset", "cleaned_dataset.rds"))
 saveRDS(traffic_data_cleaned, here::here("dataset", "cleaned_dataset.rds"))
->>>>>>> c502fd79f5e39096cf41af295f464e9114794784
