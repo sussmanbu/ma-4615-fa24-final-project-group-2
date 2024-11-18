bd56d7dfd8133c5f0e86c330463ed056c7f6d831
Jason Zhu <jzhu33@bu.edu>
2024-11-18 15:41:27 -0500

    Built site for gh-pages

--- scripts/load_and_clean_data2.R
+++ scripts/load_and_clean_data2.R
@@ -1,6 +1,7 @@
 # Load packages
 library(tidyverse)
 library(dplyr)
+library(tidyr)
 
 # Load data
 ri_traffic_data_clean <- readRDS(here::here("dataset", "cleaned_dataset.rds"))
@@ -13,7 +14,9 @@ sf_traffic_data <- readRDS(here::here("dataset", "sf_raw_dataset.rds"))
 
 ri_traffic_data_clean = ri_traffic_data_clean|>
   mutate(state = "Rhode Island",
-         driver_race = tolower(driver_race))
+         driver_race = tolower(driver_race),
+         stop_outcome = tolower(stop_outcome))|>
+  select(!violation_raw)
 
 # omitted columns are already in the dataset correctly labeled
 sf_traffic_data_clean = sf_traffic_data|>
@@ -21,14 +24,30 @@ sf_traffic_data_clean = sf_traffic_data|>
          !is.na(subject_race))|>
   mutate(state = "California",
          stop_date = date,
-         stop_time = time,
+         stop_time = as.character(time),
          driver_gender = if_else(subject_sex == 'male', 'M', 'F'),
          driver_race = if_else(subject_race == 'asian/pacific islander', 'asian', subject_race),
-         violation_raw = NULL, # differing values
-         violation = NULL, # differing values
-         search_type = NULL, # not included
-         stop_outcome = NULL, # differing values
+         violation = if_else(startsWith(reason_for_stop, "Moving Violation"), "Moving violation", "Other"),
+         search_type = if_else(T, NA, NA),
+         stop_outcome = outcome, # differing values
          is_arrested = arrest_made,
-         stop_duration = NULL, # not included,
-         drugs_related_stop = NULL, # not included,
-         )
+         stop_duration = if_else(T, NA, NA), # not included,
+         drugs_related_stop = if_else(T, NA, NA), # not included,
+         )|>
+  select(stop_date, 
+         stop_time, 
+         driver_gender, 
+         driver_race, 
+         violation,
+         search_conducted,
+         search_type, 
+         stop_outcome,
+         is_arrested,
+         stop_duration,
+         drugs_related_stop,
+         district,
+         state)
+        
+full_traffic_data_clean = full_join(ri_traffic_data_clean, sf_traffic_data_clean)
+
+saveRDS(full_traffic_data_clean, here::here("dataset", "cleaned_dataset_full.rds"))
